feddf831f38beb784f7dda66eecdd86c21c5e3c2
Pawan Gola <pawangola908@gmail.com>
2024-03-03 07:37:51 +0530

    check again id workinh

--- controllers/url.js
+++ controllers/url.js
@@ -9,11 +9,23 @@ async function handleGenerateNewShortURL(req, res) {
    await URL.create({
       shortId: shortID,
       redirectURL: body.url,
-});
+      visitHistory: [],
+   });
 
-return res.json({id: shortID });
+   return res.json({ id: shortID });
+}
+
+async function handleGetAnalytics(req, res) {
+   const shortId = req.params.shortId;
+   const result = await URL.findOne({ shortId });
+   return res.json({
+      totalClick: result.visitHistory.length, analytics: result.visitHistory,
+
+   });
+
 }
 
 module.exports = {
-       handleGenerateNewShortURL,
+   handleGenerateNewShortURL,
+   handleGetAnalytics,
 };
